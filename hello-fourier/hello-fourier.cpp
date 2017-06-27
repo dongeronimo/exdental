7026cb4fac6674d3480f4a185ebc61fe0fa131e3
Geromin <luciano.geronimo.fnord@gmail.com>
2017-06-26 22:36:52 -0300

    Log scalar rescale adicionado, saída de min/max tb

--- hello-fourier/hello-fourier.cpp
+++ hello-fourier/hello-fourier.cpp
@@ -7,21 +7,54 @@
 #include <itkInverseFFTImageFilter.h>
 #include <itkCastImageFilter.h>
 #include <itkImageFileWriter.h>
+#include <itkComplexToModulusImageFilter.h>
+#include <itkLogImageFilter.h>
+#include <itkMinimumMaximumImageFilter.h>
 using namespace std;
 
 typedef itk::Image<double, 2> FloatImageType;
 typedef itk::ForwardFFTImageFilter<FloatImageType> FFTType;
 typedef itk::InverseFFTImageFilter<FFTType::OutputImageType, FloatImageType> IFFTType;
+typedef itk::ComplexToModulusImageFilter< FFTType::OutputImageType, FloatImageType> ModulusFilterType;
+typedef itk::LogImageFilter < FloatImageType, FloatImageType > LogFilterType;
+typedef itk::MinimumMaximumImageFilter<FloatImageType> MinMaxFilterType;
 
 FloatImageType::Pointer LoadImagem();
 void SaveImagem(FloatImageType::Pointer img);
+void GetMinMax(FloatImageType::Pointer input, float& min, float& max);
+
+void PrintMinMax(float min, float max)
+{
+	wcout << "Mínimo:" << min << ", Máximo:" << max << endl;
+}
 
 int main(int argc, char** argv)
 {
 	try
 	{
+		float minValDaImagem, maxValDaImagem;
 		//1) Carrega a imagem
 		FloatImageType::Pointer image = LoadImagem();
+		//Faz a fourier
+		FFTType::Pointer fftFilter = FFTType::New();
+		fftFilter->SetInput(image);
+		fftFilter->Update();
+		// Calculo da magnitude		
+		ModulusFilterType::Pointer modulusFilter = ModulusFilterType::New();
+		modulusFilter->SetInput(fftFilter->GetOutput());
+		modulusFilter->Update();
+		GetMinMax(modulusFilter->GetOutput(), minValDaImagem, maxValDaImagem);
+		PrintMinMax(minValDaImagem, maxValDaImagem);
+		//Pelo exemplo da opencv, agora aplico log na imagem
+		LogFilterType::Pointer logFilter = LogFilterType::New();
+		logFilter->SetInput(modulusFilter->GetOutput());
+		logFilter->InPlaceOff();
+		logFilter->Update();
+		GetMinMax(logFilter->GetOutput(), minValDaImagem, maxValDaImagem);
+		PrintMinMax(minValDaImagem, maxValDaImagem);
+		//Tentar gravar pra ver o que acontece
+		
+		//Talvez eu tenha que log intensity rescale pra por em escala logaritminca
 #ifdef IR_E_VOLTAR
 		//2) Fazer a fourier da imagem e voltar para a imagem
 		FFTType::Pointer fftFilter = FFTType::New();
@@ -41,6 +74,15 @@ int main(int argc, char** argv)
 		return EXIT_FAILURE;
 	}
 	return EXIT_SUCCESS;
+}
+
+void GetMinMax(FloatImageType::Pointer input, float& min, float& max)
+{
+	MinMaxFilterType::Pointer minMax = MinMaxFilterType::New();
+	minMax->SetInput(input);
+	minMax->Update();
+	max = minMax->GetMaximum();
+	min = minMax->GetMinimum();
 }
 
 void SaveImagem(FloatImageType::Pointer img)
